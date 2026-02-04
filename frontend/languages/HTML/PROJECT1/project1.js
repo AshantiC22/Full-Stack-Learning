7313014c53f798ec92a89f6e84a6fa6015462a8f
AshantiC22 <166549526+AshantiC22@users.noreply.github.com>
2026-02-04 04:55:19 -0600

    Add files via upload

---
+++ frontend/languages/HTML/PROJECT1/project1.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button"); // Get the button element by its ID in the html file
+
+button.addEventListener("click", () => {
+  alert("Button was clicked!");
+});
